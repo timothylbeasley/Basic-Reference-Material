fe30bf2e772199956d4800564d254f5288f10e54
Timothy Beasley <59793485+timothylbeasley@users.noreply.github.com>
2020-06-23 22:33:36 -0500

    Daily PRactice Files

---
+++ simplefbcloneconcept.js
@@ -0,0 +1,35 @@
+var user1 = {
+    username: "joe",
+    password: "passmeyo",
+}
+
+var user2 = {
+    username: "joanna",
+    password: "babybaby",
+}
+
+var user3 = {
+    username: "nambones",
+    password: "sachkhand",
+}
+
+var Array, database;
+database = [user1, user2, user3];
+
+var page1 = {
+    username: "catlover",
+    timeline: "cat",
+}
+
+var page2 = {
+    username: "doglover",
+    timeline: "rufftimes",
+}
+
+var page3 = {
+    username: "birdlover",
+    timeline: "flyaway",
+}
+
+var Array, newsfeed;
+newsfeed = [page1, page2, page3];
